446f98527ee340b0d419de9177e2194cc6e040a5
savage <146668608+Rinattttt@users.noreply.github.com>
2024-10-05 13:03:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread chickenThread = new Thread(PrintChicken);
+        Thread eggThread = new Thread(PrintEgg);
+        Console.WriteLine("Спор начался! Участники спора: ");
+        chickenThread.Start();
+        eggThread.Start();
+
+        // Дожидаемся завершения обоих потоков
+        chickenThread.Join();
+        eggThread.Join();
+
+        // Проверяем, какой поток завершился последним
+        if (chickenThread.IsAlive)
+        {
+            Console.WriteLine("Побеждает: Курица");
+        }
+        else
+        {
+            Console.WriteLine("Побеждает: Яйцо");
+        }
+    }
+
+    static void PrintChicken()
+    {
+        Console.WriteLine("Курица");
+    }
+
+    static void PrintEgg()
+    {
+        Console.WriteLine("Яйцо");
+    }
+}
